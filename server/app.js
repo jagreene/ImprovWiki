04a4051d3c3f5de663b165bfe11ca8d8302ba36d
jagreene <j.austin.greene@gmail.com>
2015-03-01 08:33:14 -0500

    Wow I am so bad at reorganization, now there is a server

---
+++ server/app.js
@@ -0,0 +1,33 @@
+var express = require("express");
+var path = require("path");
+var logger = require("morgan");
+var cookieParser = require("cookie-parser");
+var bodyParser = require("body-parser");
+var exphbs  = require("express-handlebars");
+
+var index = require("./routes/index");
+var wiki  = require("./routes/wiki");
+
+var app = express();
+
+var PORT = process.env.PORT || 3001
+
+app.use(logger("dev"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", index.getIndex);
+app.get("/wiki", wiki.getArticles);
+app.get("/wiki/:id", wiki.getArticle);
+
+app.put("/wiki/:id", wiki.updateArticle);
+
+app.post("/wiki", wiki.createArticle);
+
+app.delete("/wiki/:id", wiki.deleteArticle);
+
+app.listen(PORT, function() {
+  console.log("App running on port:", PORT);
+});
