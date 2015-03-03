e9d12a2d648c3ab428a3726148ec3e611b785d6e
Greg <gredelston@gmail.com>
2015-03-03 16:23:38 -0500

    documentation

--- client/app/routes/application.js
+++ client/app/routes/application.js
@@ -1,5 +1,7 @@
 import Ember from 'ember';
 
+// Finds all article documents in the store or availble on the server
+// and pushes it to the model attribute of the template and controller
 export default Ember.Route.extend({
   model: function() {
     return this.store.find('article');
--- client/app/routes/articles/new.js
+++ client/app/routes/articles/new.js
@@ -1,11 +1,15 @@
 import Ember from 'ember';
 
+// Binds default values for name and body variables
+// in the controller and template
 export default Ember.Route.extend({
   setupController: function(controller) {
     controller.set("name", "Title");
     controller.set("body", "Body");
   },
 
+// Finds all article documents in the store or availble on the server
+// and pushes it to the model attribute of the template and controller
   model: function() {
     return this.store.find('article');
   }
