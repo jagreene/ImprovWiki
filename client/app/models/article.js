c8d00b59f3c3bbf07d4443b26bfc1c4904d0a9d7
jagreene <j.austin.greene@gmail.com>
2015-03-03 11:16:13 -0500

    mode documentation

--- client/app/models/article.js
+++ client/app/models/article.js
@@ -1,5 +1,5 @@
 import DS from 'ember-data';
-
+// Creates the article model with the attributes name, category, lastRevision, time and body
 export default DS.Model.extend({
   name: DS.attr('string'),
   category: DS.attr('string'),
